45aaee32dcd77b22ecf92152fb6988503c2c1721
maleksejev <72667389+maleksejev@users.noreply.github.com>
2020-11-15 11:22:57 +0200

    Update magic_ball.rb

--- magic_ball.rb
+++ magic_ball.rb
@@ -1,35 +1,35 @@
 #My app for magic ball
 answers = [
-    # Affirmative
-    "It is certain.",
-    "It is decidedly so.",
-    " Without a doubt.",
-    "Yes – definitely.",
-    "You may rely on it.",
-    # Affirmative
-    "As I see it, yes.",
-    "Most likely.",
-    "Outlook good",
-    "Yes.",
-    "Signs point to yes.",
-    # Non-committal
-    "Reply hazy, try again.",
-    "Ask again later.",
-    "Better not tell you now.",
-    "Cannot predict now.",
-    "Concentrate and ask again.",
-    # Negative
-    "Don't count on it.",
-    "My reply is no.",
-    "My sources say no.",
-    "Outlook not so good.",
-    "Very doubtful."
+  # Affirmative
+  "It is certain.",
+  "It is decidedly so.",
+  " Without a doubt.",
+  "Yes – definitely.",
+  "You may rely on it.",
+  # Affirmative
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good",
+  "Yes.",
+  "Signs point to yes.",
+  # Non-committal
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  # Negative
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful."
 ]
 # Data array for greetings and sample for random usage
 greetings = [
-    "Hello. Here is answer for your question...",
-    "Who ask, gets an answer:",
-    "Hello my friend. Today answer for you is:"
+  "Hello. Here is answer for your question...",
+  "Who ask, gets an answer:",
+  "Hello my friend. Today answer for you is:"
 ]
 puts greetings.sample
 sleep 4
